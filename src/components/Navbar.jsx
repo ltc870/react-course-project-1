772e67fcfc068ce914053399fefa01f194119c2c
Lawrence C <lawrencecaudle870@gmail.com>
2022-09-05 19:57:51 -0400

    added code to Navbar()

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,14 @@ import React from "react";
 
 export default function Navbar() {
     return (
-        <h1>Navbar goes here!!</h1>
+        <div className="navbar-flex">
+            <section className="navbar-child1">
+                <img src="/src/images/react-icon-small.png" alt="" />
+                <p>ReactFacts</p>
+            </section>
+            <section className="navbar-child2">
+                <p>React Course - Project 1</p>
+            </section>
+        </div>
     )
 }
