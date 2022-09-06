819a2c8f99657217a79112e0c7200cd46d480336
Lawrence C <lawrencecaudle870@gmail.com>
2022-09-05 20:15:18 -0400

    rearranged components

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Navbar from "./components/Navbar";
 export default function App() {
     return (
       <div className="container">
-        <Main />
         <Navbar />
+        <Main />
       </div>
     );
   }
