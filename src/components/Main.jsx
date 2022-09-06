57a923751836aed4ab25312e9676e6524e6b209c
Lawrence C <lawrencecaudle870@gmail.com>
2022-09-05 21:04:11 -0400

    added code to Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,16 @@ import React from "react";
 
 export default function Main() {
     return (
-        <h1>Main goes here!</h1>
+        <main className="main">
+            <h1 className="main-title">Fun Facts about React</h1>
+            <ul className="main-list">
+                <li className="main-list-item">Was first released in 2013</li>
+                <li className="main-list-item">Was originally created by Jordan Walke</li>
+                <li className="main-list-item">Has well over 100K stars on GitHub</li>
+                <li class="main-list-item">Is maintained by Facebook</li>
+                <li class="main-list-item">Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+        
     )
 }
