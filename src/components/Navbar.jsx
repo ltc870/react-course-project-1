547e60ff8b0f8f240dfab9854f68532bc2472584
Lawrence C <lawrencecaudle870@gmail.com>
2022-09-05 20:14:49 -0400

    added styles to Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,11 @@ export default function Navbar() {
     return (
         <div className="navbar-flex">
             <section className="navbar-child1">
-                <img src="/src/images/react-icon-small.png" alt="" />
-                <p>ReactFacts</p>
+                <img className="navbar-child1-logo" src="/src/images/react-icon-small.png" alt="" />
+                <p className="navbar-child1-text">ReactFacts</p>
             </section>
             <section className="navbar-child2">
-                <p>React Course - Project 1</p>
+                <p className="navbar-child2-text">React Course - Project 1</p>
             </section>
         </div>
     )
